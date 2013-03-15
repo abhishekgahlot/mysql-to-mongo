12d8a5d7735de28e0a9e07e9dbd15dcbfdd2b190
AbhishekGahlot <abhishekgahlot007@gmail.com>
2013-03-15 19:40:52 +0530

    Index.php
    Php file to convert mysql to mongodb.
    You have to run this file on browser or using telnet.
    You may also try php index.php . But this will sometime create error which would be
    PHP Fatal error:  Class 'MongoClient'
    This is caused when you don't have mongo driver installed or there might be problem with
    API module of php.
    
    In this file you have to change only these things:
    
    define('dbname', 'YOUR_DB_NAME');
    private $dbhost='DATABASE_HOST'; 
    private $dbusername='DATABASE_USERNAME';
    private $dbpassword='DATABASE_PASSWORD';
    
    Dbname for both mongo and mysql should be same
    Dont worry if mongodb database is not present. It will automatically create for you with all the collection details.
    
    NOTE: This converts all the data to string from mysql to mongo.

---
+++ index.php
@@ -0,0 +1,122 @@
+<?php
+/*Created by
+Abhishek Gahlot On  March 2013
+http://www.abhishekgahlot.com
+http://www.cloudtub.com
+
+Mysql To Mongodb Importer
+
+*/
+define('dbname', 'YOUR_DB_NAME'); //Enter database name of mysql. Mongo will also create the database of this name.
+
+//db connection class using singleton pattern
+class dbConn{
+
+private $dbhost='DATABASE_HOST'; //Enter mysql database host
+private $dbusername='DATABASE_USERNAME';  //Enter mysql database username
+private $dbpassword='DATABASE_PASSWORD'; //Enter mysql database password
+
+//variable to hold connection object.
+protected static $db;
+
+//private construct
+private function __construct() {
+
+try {
+// assign PDO object to db variable
+self::$db = new PDO('mysql:host='.$this->dbhost.';dbname='.dbname, $this->dbusername, $this->dbpassword );
+self::$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+//Output error - would normally log this to error file rather than output to user.
+echo "Connection Error: " . $e->getMessage();
+}
+
+}
+
+// get connection function. Static method - accessible without instantiation
+public static function getConnection() {
+
+//Guarantees single instance, if no connection object exists then create one.
+if (!self::$db) {
+//new connection object.
+new dbConn();
+}
+
+//return connection.
+return self::$db;
+}
+
+}//end class
+
+
+class Mysql{
+
+  public function showTables(){
+  			$dbname=dbname;
+				$db = dbConn::getConnection();
+	            $query=$db->query("SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_TYPE='BASE TABLE' AND TABLE_SCHEMA='$dbname'");
+				//$query=$db->query('SELECT * FROM users');
+				while($results = $query->fetch(PDO::FETCH_ASSOC)){
+					foreach($results as $key=>$val){
+					//Tables name
+						$tableArray[]=$val;
+					}
+
+				}
+				//Send tables name data to columns function
+				$this->showColumns($tableArray);
+	}
+
+
+
+	private function showColumns($tableArray){
+				$db = dbConn::getConnection();
+				$dbname=dbname;
+
+		 foreach($tableArray as $table=>$tableName){//foreach for every table name fetch table name
+
+	           $query=$db->query("SELECT `COLUMN_NAME` FROM `INFORMATION_SCHEMA`.`COLUMNS` WHERE `TABLE_SCHEMA`='$dbname'  AND `TABLE_NAME`='$tableName'");
+	           		unset($columnArray);//unset columnarray so that previous values dont get mixed
+
+					while($columnQuery = $query->fetch(PDO::FETCH_ASSOC)){
+					foreach($columnQuery as $column=>$columnname){//foreach for every table name fetch column name
+					//columns name
+					   $columnArray[]=$columnname;
+					}
+
+				}   //Send columns name and Table name to Data function
+						$this->showData($tableName,$columnArray);
+		}
+
+	}//function show columns ends here
+
+
+
+	private function showData($tableName,$columnArray){
+
+		 $dbname=dbname;
+		 $MongoConnect = new MongoClient();
+		 $db = $MongoConnect->$dbname;
+		 $tableCreation = $db->createCollection($tableName);
+
+				$db = dbConn::getConnection();
+	            $query=$db->query("SELECT * from `$tableName`");
+
+				while($results = $query->fetch(PDO::FETCH_ASSOC)){
+
+
+							$rowColumn_combined=array_combine($columnArray, $results);//combining column and data to insert
+								 $tableCreation->insert($rowColumn_combined);
+
+				}
+		echo 'Successfully Imported Table '.$tableName."\n";
+
+	}//showdata function bracket
+
+}
+$obj=new mysql;
+$obj->showTables();
+
+
+?>
